69f2b0700197cf4aab3cbaaf1ea765dfe14c000f
De'Jour <dejourford@yahoo.com>
2026-01-25 20:38:06 -0600

    render task section title by active tab id

--- src/ui/events.js
+++ src/ui/events.js
@@ -1,9 +1,25 @@
 export default function initAppEvents() {
 
+    // create function to show respective content when clicked
+    function showTabContent(tabText, id) {
+        const taskSection = document.querySelector(".task-section");
+        taskSection.id = id;
+
+        // remove previous tabText content
+        taskSection.innerHTML = ""
+
+        // create section title
+        const taskSectionTitle = document.createElement("h2");
+        taskSectionTitle.classList.add("task-title");
+        taskSectionTitle.textContent = tabText;
+
+        // assembly
+        taskSection.appendChild(taskSectionTitle)
+    }
+
     // listener for active navigation tab clicks
     document.querySelectorAll(".nav-item").forEach((item) => {
         item.addEventListener("click", () => {
-            
         
             document.querySelectorAll(".nav-item").forEach((nav) => {
                 nav.classList.remove("active");
@@ -12,6 +28,11 @@ export default function initAppEvents() {
 
             // add active class to clicked button
             item.classList.add("active");
+
+            // show tab content
+            const tabTextContent = item.textContent
+            const tabId = item.id
+            showTabContent(tabTextContent, tabId);
         })
     })
 }
--- src/ui/render.js
+++ src/ui/render.js
@@ -5,94 +5,100 @@ import thisWeek from "../assets/icons/this-week.svg"
 import upcoming from "../assets/icons/upcoming.svg"
 import completed from "../assets/icons/completed.svg"
 
+function renderNavigation() {
+    // page title
+    const title = document.createElement("h1");
+    title.textContent = "todolist";
+
+    // sidebar
+    const sidebar = document.createElement("div");
+    sidebar.classList.add("sidebar");
+
+    // navigation
+    const nav = document.createElement("nav");
+    nav.role = "navigation";
+
+    // navigation items
+    const navItems = [
+        {
+            "id": "inbox",
+            "title": "Inbox",
+            "imgPath": inbox,
+        },
+        {
+            "id": "today",
+            "title": "Today",
+            "imgPath": today,
+        },
+        {
+            "id": "tomorrow",
+            "title": "Tomorrow",
+            "imgPath": tomorrow,
+        },
+        {
+            "id": "thisWeek",
+            "title": "This Week",
+            "imgPath": thisWeek,
+        },
+        {
+            "id": "upcoming",
+            "title": "Upcoming",
+            "imgPath": upcoming,
+        },
+        {
+            "id": "completed",
+            "title": "Completed",
+            "imgPath": completed,
+        },
+    ]
+
+    const ulElement = document.createElement("ul");
+
+    navItems.forEach((item) => {
+        console.log(item)
 
 
+        // <ul>
+        // <li>
+        // <img></img>
+        // <p></p>
+        // </li>
+        // </ul>
 
+        // nav item element (li)
+        const navItem = document.createElement("li");
+        navItem.classList.add("nav-item");
+        navItem.id = item.id;
 
-    function renderNavigation() {
-        // page title
-        const title = document.createElement("h1");
-        title.textContent = "todolist";
+        // img for nav item (img)
+        const img = document.createElement("img");
+        img.src = item.imgPath;
+        img.classList.add(`${item.imgPath}-icon`)
 
-        // sidebar
-        const sidebar = document.createElement("div");
-        sidebar.classList.add("sidebar");
-
-        // navigation
-        const nav = document.createElement("nav");
-        nav.role = "navigation";
-
-        // navigation items
-        const navItems = [
-            {
-                "title": "Inbox",
-                "imgPath": inbox,
-            },
-            {
-                "title": "Today",
-                "imgPath": today,
-            },
-            {
-                "title": "Tomorrow",
-                "imgPath": tomorrow,
-            },
-            {
-                "title": "This Week",
-                "imgPath": thisWeek,
-            },
-            {
-                "title": "Upcoming",
-                "imgPath": upcoming,
-            },
-            {
-                "title": "Completed",
-                "imgPath": completed,
-            },
-        ]
-
-        const ulElement = document.createElement("ul");
-
-        navItems.forEach((item) => {
-            console.log(item)
-
-
-            // <ul>
-            // <li>
-            // <img></img>
-            // <p></p>
-            // </li>
-            // </ul>
-
-            // nav item element (li)
-            const navItem = document.createElement("li");
-            navItem.classList.add("nav-item");
-            navItem.id = item.title;
-
-            // img for nav item (img)
-            const img = document.createElement("img");
-            img.src = item.imgPath;
-            img.classList.add(`${item.imgPath}-icon`)
-
-            // nav item text (p)
-            const navItemText = document.createElement("p");
-            navItemText.classList.add("nav-item-text")
-            navItemText.textContent = item.title;
-
-            // assembly
-            navItem.append(img, navItemText)
-            ulElement.append(navItem)
-        })
-
+        // nav item text (p)
+        const navItemText = document.createElement("p");
+        navItemText.classList.add("nav-item-text")
+        navItemText.textContent = item.title;
 
         // assembly
-        nav.append(ulElement)
-        sidebar.append(title, nav)
+        navItem.append(img, navItemText)
+        ulElement.append(navItem)
+    })
 
-        return sidebar
-    }
 
-    export default function renderApp() {
-        const app = document.querySelector("#app");
-        app.innerHTML = "";
-        app.append(renderNavigation());
-    }
+    // assembly
+    nav.append(ulElement)
+    sidebar.append(title, nav)
+
+    return sidebar
+}
+
+export default function renderApp() {
+    const app = document.querySelector("#app");
+    app.innerHTML = "";
+
+    const taskSection = document.createElement("main");
+    taskSection.classList.add("task-section");
+    
+    app.append(renderNavigation(), taskSection);
+}
